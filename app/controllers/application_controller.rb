270af3ba6fcd3d0a6cfd2f74ced7253f05f9ded8
amira <amira.a.a.hashim@gmail.com>
2020-04-26 18:32:27 +0200

    callback for access denied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,9 @@
 class ApplicationController < ActionController::Base
     # before_action :authenticate_user!
-    # before_action :configure_permitted_parameters, if: :devise_controller?
-
-    # protected
-  
-    # def configure_permitted_parameters
-    #   devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation])
-    # end
-
-    # before_action :configure_permitted_parameters, if: :devise_controller?
-
-    # protected
-    #     def configure_permitted_parameters
-    #         devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
-    #         devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
-    #         devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
-    #     end 
+    rescue_from CanCan::AccessDenied do |exception|
+        respond_to do |format|
+          format.json { head :forbidden }
+          format.html { redirect_to main_app.root_url, :alert => exception.message }
+        end
+      end
 end
